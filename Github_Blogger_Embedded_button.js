64ea72a0b54f66a574675d4ee3e0f87147097e45
Cha0smagick <alekos20@gmail.com>
2023-07-24 12:43:01 -0500

    Create Github_Blogger_Embedded_button.js
    Button 1.0

---
+++ Github_Blogger_Embedded_button.js
@@ -0,0 +1,2 @@
+<center><script src="https://buttons.github.io/buttons.js"></script>
+<a class="github-button" href="https://github.com/github_username" data-size="large" data-show-count="true" aria-label="Follow github_username on GitHub">Follow @github_username</a></center>
