096f3d691c5ab6eb63cd44ba041b830729c198ec
Cha0smagick <alekos20@gmail.com>
2023-07-24 12:50:00 -0500

    Create twitter_embedded_button.js

---
+++ twitter_embedded_button.js
@@ -0,0 +1,2 @@
+<center><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+<a class="twitter-follow-button" href="https://twitter.com/username" data-size="large" data-show-count="true">Follow @twitter_username</a></center>
