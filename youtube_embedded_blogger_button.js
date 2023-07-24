e64ca7a523a43acf4ed5f541d5c3304f678670d2
Cha0smagick <alekos20@gmail.com>
2023-07-24 18:47:40 -0500

    Create youtube_embedded_blogger_button.js

---
+++ youtube_embedded_blogger_button.js
@@ -0,0 +1,2 @@
+<center><script src="https://apis.google.com/js/platform.js"></script>
+<div class="g-ytsubscribe" data-channelid="ID" data-layout="full" data-theme="dark" data-count="undefined"></div></center>
